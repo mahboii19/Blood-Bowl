cba4d7cae33e9b46920e9fb6840b9d585eae974c
Mahboii19 <bryancardenas712@gmail.com>
2026-02-15 21:48:53 -0600

    Added a scraper .py module for retailer - flipside gaming

---
+++ scraper/flipside_gaming.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive"
+}
+
+def fetch_flipside_gaming_price(url: str):
+    """
+    Scrapes flipside gaming product URL contained in products.json config file.
+    Returns the price as a string, or None if not found.
+    """
+
+    try:
+        r = requests.get(url, headers=HEADERS, timeout=15)
+        r.raise_for_status()
+    except Exception as e:
+        print(f"[FP ERROR] Failed to fetch {url}: {e}")
+        return None
+
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    # -------------------------
+    # PRICE (your exact selector)
+    # -------------------------
+
+    # Targeting the price <div> directly
+    price_el = soup.select_one("div.t4s-product-price")
+
+    # If the price element is not found, price_el = None and if not None == True, so we return None
+    if not price_el:
+        return None
+
+    return price_el.get_text(strip=True)
